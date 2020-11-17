751137810023d5364fdb079b3d0611e778793262
Ubuntu <ubuntu@ip-172-31-93-43.ec2.internal>
2020-11-17 15:57:28 +0000

    Lab - configuration an app

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -3,6 +3,14 @@ package io.pivotal.pal.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// What are the Spring Boot Features
+//  -   Handles the cration of boiler-plate code
+//  -   Auto configuration
+//  -   Dependency management
+//  -   Fat jar (built in web-container)
+//  -   Introduce annotation functionality
+//      -   Scans classes for beans (classes)
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,12 +1,19 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    private final String localMessage;
+
+    public WelcomeController(@Value("${welcome.message}") String message) {
+        this.localMessage = message;
+    }
+
     @GetMapping("/")
     public String sayHello(){
-        return "hello";
+        return localMessage;
     }
 }
