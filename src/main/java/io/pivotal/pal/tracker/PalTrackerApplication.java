abdd96faab6e9545d712b9265031a079ff958693
Ubuntu <ubuntu@ip-172-31-93-43.ec2.internal>
2020-11-19 16:34:48 +0000

    Spring JDBC lab

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 // What are the Spring Boot Features
 //  -   Handles the cration of boiler-plate code
 //  -   Auto configuration
@@ -15,12 +17,17 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class PalTrackerApplication {
 
+    private DataSource dataSource;
+    
     public static void main(String[] args){
         SpringApplication.run(PalTrackerApplication.class, args);
+    }
+    public PalTrackerApplication(DataSource datasource){
+        this.dataSource = datasource;
     }
 
     @Bean
     TimeEntryRepository timeEntryRepository(){
-        return new InMemoryTimeEntryRepository();
+        return new JdbcTimeEntryRepository( dataSource );
     }
 }
